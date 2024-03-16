e8e55fee61203d62c67f0178fe1c9d481388187a
Stefan Erwin Baumer <baumeredv@gmail.com>
2024-03-16 16:06:12 +0100

    introduced option to turn off screen clearing, especially useful if a machine doesnt offer tput

--- bashortcut.sh
+++ bashortcut.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 bashortcut(){
-    local shortcutKeys="hjklasdfguiopqwertzbnmyxcv"
+    local SHORTCUT_KEYS="hjklasdfguiopqwertzbnmyxcv"
+    local DO_CLEAR_MENU=true
     
     local i
-    for i in $(seq 0 ${#shortcutKeys})
-        do local shortcuts[$i]=${shortcutKeys:$i:1}
+    for i in $(seq 0 ${#SHORTCUT_KEYS})
+        do local shortcuts[$i]=${SHORTCUT_KEYS:$i:1}
     done
     
     local commands
@@ -27,15 +28,15 @@ bashortcut(){
     local selectedKey
     read -rsn1 selectedKey
     
-    local currentLine
-    local currentColumn
-    IFS='[;' read -p $'\e[6n' -d R -rs _ currentLine currentColumn _ # https://www.reddit.com/r/bash/comments/139hvju/finding_the_current_cursor_position_in_bash/
-    
-    local deleteLineStart=$(( ${currentLine} - ${offerCount} - 2))
-    local deleteLineStart=$(( deleteLineStart < 0 ? 0 : deleteLineStart ))
-    
-    tput cup $deleteLineStart 0
-    tput ed
+    if [ "$DO_CLEAR_MENU" = true ] ; then 
+        local currentLine
+        local currentColumn
+        IFS='[;' read -p $'\e[6n' -d R -rs _ currentLine currentColumn _ # https://www.reddit.com/r/bash/comments/139hvju/finding_the_current_cursor_position_in_bash/
+        local deleteLineStart=$(( ${currentLine} - ${offerCount} - 2))
+        local deleteLineStart=$(( deleteLineStart < 0 ? 0 : deleteLineStart ))
+        tput cup $deleteLineStart 0
+        tput ed
+    fi
     
     for ((i=0; i<$offerCount; ++i)) do
         if [ ${selectedKey} == ${shortcuts[${i}]} ]; then 
